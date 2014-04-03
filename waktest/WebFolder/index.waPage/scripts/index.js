d2689868ee1bfa4b7c2a5872ed3df3ad32fdaaa3
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-04-03 12:55:21 +0200

    Button hinzugefügt

---
+++ waktest/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var buttonName = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	buttonName.click = function buttonName_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		var name = $$("textFieldName").getValue();
+		$$("richTextNameAusgeben").setValue(name);
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("buttonName", "click", buttonName.click, "WAF");
+// @endregion
+};// @endlock
